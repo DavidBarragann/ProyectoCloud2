706f13e13c0a6963f04d7d55f59d54276475186c
DavidBarragann <dbarragan11@alumnos.uaq.mx>
2025-03-17 18:41:45 -0600

    commit1

--- .venv/main.py
+++ .venv/main.py
@@ -4,9 +4,27 @@ import io
 from dotenv import load_dotenv
 from pydub import AudioSegment
 from pydub.playback import play
+import pygame
+
 
 # Ruta del archivo de audio
 video = 'C:/Users/david/Downloads/audioejemplo.wav'
+
+# Reproducir el audio original antes de la transcripción
+print("Reproduciendo audio original...")
+
+# Inicializa el mixer de pygame
+pygame.mixer.init()
+
+# Cargar el archivo de audio .wav
+pygame.mixer.music.load(video)
+
+# Reproducir el archivo de audio
+pygame.mixer.music.play()
+
+# Mantener el programa corriendo mientras se reproduce el audio
+while pygame.mixer.music.get_busy():
+    pygame.time.Clock().tick(10)
 
 # Claves y endpoint de Azure
 API_KEY = '6JtsnBbBD1vt3NGn1enHJUumR3LmW8suTsQZASQnXtph8tb9hoJCJQQJ99BCACYeBjFXJ3w3AAAYACOGRqV0'
@@ -28,6 +46,9 @@ synth_output_config = speechsdk.audio.AudioOutputConfig(use_default_speaker=True
 translation_config.speech_synthesis_voice_name = 'en-US-AvaMultilingualNeural'
 speech_synthesizer = speechsdk.SpeechSynthesizer(speech_config=speech_config, audio_config=synth_output_config)
 
+# Reproducir el audio original antes de la transcripción
+print("Realizando la traducción")
+
 # Inicialización del reconocedor de traducción
 recognizer = speechsdk.translation.TranslationRecognizer(
     translation_config=translation_config, audio_config=audio_config)
@@ -41,7 +62,7 @@ if result.reason == speechsdk.ResultReason.TranslatedSpeech:
     print(f"Texto en español: {source_language_text}")
     print(f"Traducción al inglés: {result.translations['en']}")
 
-    # Síntesis de voz
+    # Síntesis de voz del texto traducido
     speech_synthesis_result = speech_synthesizer.speak_text_async(result.translations['en']).get()
 
 elif result.reason == speechsdk.ResultReason.NoMatch:
